dce99160188cf72c5aa1dbd974afed7b2d113f01
Said Oliveira Aoun <42614680+saidoliveira@users.noreply.github.com>
2018-10-10 15:15:08 -0300

    Add files via upload

--- keno.cpp
+++ keno.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <random>
 #include "keno.h"
-
+#include <ctime> 
+#include <cstdlib>
+#include <random>
 using namespace std;
 
-int main(){
-	ifstream file("keno.txt");
+int rand_number(){
+	random_device rd;  //Will be used to obtain a seed for the random number engine
+    mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    uniform_int_distribution<> dis(1, 80);
 
+	return round(dis(gen));
+}
+
+int main(){
+
+	ifstream file("keno.txt");
 	KenoBet Keno;
 	int cont = 1;
 	int value;
-	vector<unsigned short int> numeros;
+	vector<number_type> numeros;
+	vector<number_type> numerosRandoms;
+	numerosRandoms.reserve(20);
+	
 	while(true){
 		if(!(file >> value)){
 			break;
@@ -26,17 +40,26 @@ int main(){
 		if(!file.eof()){
 			cont++;
 		}
-	}
+	}	
 
 	Keno.setApostas(numeros);
 
-	cout << Keno.getCredito() << endl;
-	cout << Keno.getRodadas() << endl;
-
-	int tam = numeros.size();
-	for(int i = 0; i < tam; i++){
+	cout <<"Credito do jogador: " << Keno.getCredito() << endl;
+	cout <<"Numero de rodadas: " << Keno.getRodadas() << endl;
+	cout <<"Numeros apostados: ";
+	for(number_type i = 0; i < numeros.size(); i++){
 		cout << numeros[i] << " ";
 	}
-	
+	cout << endl;
+
+	cout << "Numeros sorteados: " << endl;
+	for(number_type k = 0; k < Keno.getRodadas(); k++){
+		for(number_type j = 0; j < 20; j++){
+			numerosRandoms[j] = rand_number();
+			cout << numerosRandoms[j] << ' '; 
+		}
+		cout << endl;
+	}
+
 	return 0;
 } 
--- keno.h
+++ keno.h
@@ -1,33 +1,39 @@
 #ifndef KENO_H
 #define KENO_H
-
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <ctime> 
+#include <cstdlib>
 using number_type = unsigned short int; //<! data type for a keno hit.
 
-class randomNumber{
-public:
-	int randomNumberGenerator(int min, int max){
-		return (rand() % max + min);
-	}
-};
+
 
 class KenoBet{
 	private:
 		float credito;                    //<!The player's wage.
 		number_type rodadas;         	  //<!The player's rounds.
 		std::vector<number_type> apostas; //<!The player's bet.
+		std::vector<number_type> apostafinal;
+		std::vector<number_type> numerosSorteados;
 
 	public:
 		float getCredito();
 		void setCredito(float x);
 		number_type getRodadas();
 		void setRodadas(number_type x);
-		void setApostas(std::vector<number_type> &apostas);
 
-		bool verificaNumero(number_type num);
-		bool verificaCredito(float apostaDada);
+		bool verificaNumero(number_type num);//Verifica se o numero selecionado está no vetor
+		bool verificaCredito(float apostaDada);//Verifica sea aposta é válida
+		void reset(void);//Zera a aposta(?)
+		
+		void setApostas(std::vector<number_type> &apostas);//Recebe um vetor com as apostas dadas pelo jogador no arquivo
+		void randomNumber(std::vector<number_type> &numerosSorteados);
 
 		
 };
+
+//void KenoBet::randomNumber(std::vector<number_type> &numerosSorteados){}
 
 float KenoBet::getCredito(){
 	return credito;
@@ -44,8 +50,11 @@ void KenoBet::setRodadas(number_type x){
 
 void KenoBet::setApostas(std::vector<number_type> &apostas){}
 
+
+
+
 bool KenoBet::verificaNumero(number_type num){
-	for(int i = 0; i < apostas.size(); i++){
+	for(number_type i = 0; i < apostas.size(); i++){
 		if(apostas[i] == num){
 			return false;
 		}
@@ -53,11 +62,11 @@ bool KenoBet::verificaNumero(number_type num){
 	return true;
 }
 
-bool verificaCredito(float credito){
+bool KenoBet::verificaCredito(float credito){
 	return credito > 0;
 }
 
-void reset(void){
+void KenoBet::reset(){
 	credito = 0;
 }
 
